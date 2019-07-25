0010fa7df7fbbb88085b5089d60d12f1348f477a
Dan SG <dan@cloudgenix.com>
2019-07-25 15:04:22 -0700

    add demo app

--- static/js/cgx.js
+++ static/js/cgx.js
@@ -5,6 +5,11 @@ $(document).ready(() => {
     $('#run_button').button();
     $('#load_button').button();
     $('#show_button').button();
+    
+    let workspace = Blockly.getMainWorkspace();
+    workspace.clear();
+    CGX_save=Blockly.Xml.textToDom('<xml xmlns="http://www.w3.org/1999/xhtml"><variables><variable type="" id="w6Z|M.J3UNMr92N%!PF/">element</variable></variables><block type="cgx_token" id="hzJ@#g(T;exkz@GE?_,f" x="116" y="50"><value name="TOKEN"><block type="text" id="MD2)Kuj-@c!a?cey#m0Y"><field name="TEXT">PASTE TOKEN HERE</field></block></value><next><block type="cgx_output" id="G%kCtGPGYb}47@3(0{*I"><value name="OUTPUT"><block type="text" id="`%;@Z6SK4`4UX/Jnim#g"><field name="TEXT">demo that will list all elements</field></block></value><next><block type="controls_forEach" id="kiA0~1w1l!A72`(DlOZV"><field name="VAR" id="w6Z|M.J3UNMr92N%!PF/" variabletype="">element</field><value name="LIST"><block type="cgx_get_object_list" id="Z8J`FP/dvCsEhAF8|Y5)"><field name="NAME">elements</field></block></value><statement name="DO"><block type="cgx_output" id="d-i4qTG5~v(h+MA4}@ur"><value name="OUTPUT"><block type="cgx_element_attribute" id="^i;;]2Z=PiMXwN@])n8u"><field name="FIELD">name</field><value name="ELEMENT"><block type="variables_get" id="S:Fx6T|3juEpP{UKBg?8"><field name="VAR" id="w6Z|M.J3UNMr92N%!PF/" variabletype="">element</field></block></value></block></value></block></statement></block></next></block></next></block></xml>');
+    Blockly.Xml.domToWorkspace(CGX_save, workspace);
 });
 
 
@@ -51,9 +56,9 @@ $('#load_button').on('click',(event)=>{
 });
 
 function blink_run() {
-    $('#run_button').fadeOut(300);
-    $('#run_button').fadeIn(600);
     if (CGX_blink_run) {
+        $('#run_button').fadeOut(300);
+        $('#run_button').fadeIn(600);
         setTimeout(blink_run,900)
     }
 }
